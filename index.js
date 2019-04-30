eb1cd31872634a38430ac6d56625ebc0a36484d3
Ryan Silva <RyandotSilva@gmail.com>
2019-04-30 18:55:06 -0400

    Add logic to parse data into array

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 const fs = require('fs');
+
+fs.readFile('load_salaries.txt', 'utf8', (err, data) => {
+	// Read data from file. Split the data at every ( and turn into an array.
+	const arrayData = data.split('(');
+
+	const splitArray = [];
+	for (var i = 0; i < arrayData.length; i++) {
+		// Loop through array and push items into a new array splitting at the ,
+		splitArray.push(arrayData[i].split(','));
+	}
+});
