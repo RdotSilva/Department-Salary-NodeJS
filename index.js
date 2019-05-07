d8d3aaf1fd57e14d3ea95d857a3a9b203acd48e9
Ryan Silva <RyandotSilva@gmail.com>
2019-05-06 20:24:13 -0400

    Add comments

--- index.js
+++ index.js
@@ -3,12 +3,15 @@ const fs = require('fs');
 fs.readFile('load_salaries.txt', 'utf8', (err, data) => {
 	// Read data from file. Split the data at every ( and turn into an array.
 	const arrayData = data.split('(');
+	console.log(arrayData);
 
 	const splitArray = [];
 	for (let i = 0; i < arrayData.length; i++) {
 		// Loop through array and push items into a new array splitting at the ,
 		splitArray.push(arrayData[i].split(','));
 	}
+
+	console.log(splitArray);
 
 	// Represents employees who are currently working.
 	const currentlyEmployed = "'9999-01-01')";
@@ -20,13 +23,14 @@ fs.readFile('load_salaries.txt', 'utf8', (err, data) => {
 			currentEmployees.push(splitArray[i]);
 		}
 	}
+	console.log(currentEmployees);
 
 	// Clean up array (remove '\r\n' from arrays)
 	for (let i = 0; i < currentEmployees.length; i++) {
 		currentEmployees[i].pop();
 	}
 
-	const totalSalary = 0;
+	let totalSalary = 0;
 	for (let i = 0; i < currentEmployees.length; i++) {
 		// Add the salary of each current employee together.
 		totalSalary += parseInt(currentEmployees[i][1]);
@@ -38,6 +42,7 @@ fs.readFile('load_salaries.txt', 'utf8', (err, data) => {
 	fs.readFile('load_dept_emp.txt', 'utf8', (err, data) => {
 		// Read data from file. Split the data at every ( and turn into an array.
 		const departmentArray = data.split('(');
+
 		const newDepartmentArray = [];
 		for (let i = 0; i < departmentArray.length; i++) {
 			// Loop through array and push items into a new array splitting at the ,
@@ -46,6 +51,7 @@ fs.readFile('load_salaries.txt', 'utf8', (err, data) => {
 
 		// Represents employees who are currently working.
 		const currentlyEmployeed = "'9999-01-01')";
+
 		const currentEmployeesByDepartment = [];
 		for (let i = 0; i < newDepartmentArray.length; i++) {
 			if (newDepartmentArray[i][3] === currentlyEmployeed) {
@@ -77,47 +83,59 @@ fs.readFile('load_salaries.txt', 'utf8', (err, data) => {
 			}
 		}
 
+		console.log(departmentWithSalaries);
+
 		// Department salary totals
-		let marketing = 0,
-			finance = 0,
-			humanResources = 0,
-			production = 0,
-			development = 0,
-			qualityManagement = 0,
-			sales = 0,
-			research = 0,
-			customerService = 0;
+		let marketing = 0;
+		let finance = 0;
+		let humanResources = 0;
+		let production = 0;
+		let development = 0;
+		let qualityManagement = 0;
+		let sales = 0;
+		let research = 0;
+		let customerService = 0;
 
 		for (let i = 0; i < departmentWithSalaries.length; i++) {
 			const department = departmentWithSalaries[i][2];
+
 			switch (department) {
 				case "'d001'":
 					marketing =
 						marketing + parseInt(departmentWithSalaries[i][1]);
+					break;
 				case "'d002'":
 					finance = finance + parseInt(departmentWithSalaries[i][1]);
+					break;
 				case "'d003'":
 					humanResources =
 						humanResources + parseInt(departmentWithSalaries[i][1]);
+					break;
 				case "'d004'":
 					production =
 						production + parseInt(departmentWithSalaries[i][1]);
+					break;
 				case "'d005'":
 					development =
 						development + parseInt(departmentWithSalaries[i][1]);
+					break;
 				case "'d006'":
 					qualityManagement =
 						qualityManagement +
 						parseInt(departmentWithSalaries[i][1]);
+					break;
 				case "'d007'":
 					sales = sales + parseInt(departmentWithSalaries[i][1]);
+					break;
 				case "'d008'":
 					research =
 						research + parseInt(departmentWithSalaries[i][1]);
+					break;
 				case "'d009'":
 					customerService =
 						customerService +
 						parseInt(departmentWithSalaries[i][1]);
+					break;
 			}
 		}
 		console.log(
